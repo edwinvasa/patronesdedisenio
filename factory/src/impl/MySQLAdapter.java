5ba72be045d69c728f58ae194432d89b9f333cf4
Edwin Vanegas <edwinvasa@gmail.com>
2021-10-18 21:13:23 -0500

    se crea la interfaz IDBAdapter la cual define la estructura de los productos que prodrá crear el Factory, en este ejemplo hay dos clases concretas una para Postgresql y otra para MySQL. define el metodo getConnection el cual crea las conexiones a la base de datos, las clases concretas deberán implementar la lógica para realizar esta conexión.
    se crea PostgresAdapter.java la cual implementa la interfaz IDBAdapter y el metodo getConnection para retornar la conexion abierta a la BD PostgreSQL. dentro esta el bloque estatico con esto nos aseguramos de que driver JDBC de postgres sea registrado antes de que el metodo getConnection sea ejecutado; para la cadena de conexión usamos el metodo createConnectionString para que nos retorne la conexión apropiada para conectarnos a Postgres. se utiliza la clase PropertiesUtil.java, la cual se encarga de leer el .properties para obtener los parametros de conexión.

--- factory/src/impl/MySQLAdapter.java
+++ factory/src/impl/MySQLAdapter.java
@@ -1,4 +1,58 @@
 package impl;
 
-public class MySQLAdapter {
+import db.IDBAdapter;
+import util.PropertiesUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class MySQLAdapter implements IDBAdapter {
+
+
+    //ruta donde se encuentra el archivo .properties con las datos de conexion a la BD
+    private static final String DB_PROPERTIES = "META-INF/DBMySql.properties";
+
+    //propiedades de los archivos properties
+    private static final String DB_NAME = "dbname";
+    private static final String DB_HOST = "host";
+    private static final String DB_PORT = "port";
+    private static final String DB_USER = "user";
+    private static final String DB_PASSWORD = "password";
+
+    //bloque estatico para registrar el driver de Postgres
+    static {
+        try{
+            new com.mysql.jdbc.Driver();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Connection getConnection() {
+        try {
+            String connectionString = createConnectionString();
+            Connection connection = DriverManager.getConnection(connectionString);
+            System.out.println("connection class = " + connection.getClass().getName());
+            return null;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private String createConnectionString() {
+        Properties prop = PropertiesUtil.loadProperty(DB_PROPERTIES);
+        String host = prop.getProperty(DB_HOST);
+        String db = prop.getProperty(DB_NAME);
+        String port = prop.getProperty(DB_PORT);
+        String user = prop.getProperty(DB_USER);
+        String password = prop.getProperty(DB_PASSWORD);
+
+        String connectionString = "jdbc:postgresql://" + host + ":" + port + "/"
+                + db + "?user=" + user + "&password=" + password;
+        System.out.println("connectionString = " + connectionString);
+        return connectionString;
+    }
 }
--- factory/src/impl/PostgresAdapter.java
+++ factory/src/impl/PostgresAdapter.java
@@ -1,4 +1,60 @@
 package impl;
 
-public class PostgresAdapter {
+import db.IDBAdapter;
+import util.PropertiesUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+
+public class PostgresAdapter implements IDBAdapter {
+
+    //ruta donde se encuentra el archivo .properties con las datos de conexion a la BD
+    private static final String DB_PROPERTIES = "META-INF/DBPostgres.properties";
+
+    //propiedades de los archivos properties
+    private static final String DB_NAME = "dbname";
+    private static final String DB_HOST = "host";
+    private static final String DB_PORT = "port";
+    private static final String DB_USER = "user";
+    private static final String DB_PASSWORD = "password";
+
+    //bloque estatico para registrar el driver de Postgres,
+    // con esto nos aseguramos de que driver JDBC de postgres sea registrado
+    // antes de que el metodo getConnection sea ejecutado
+    static {
+        try {
+            new org.postgresql.Driver();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Connection getConnection() {
+        try {
+            String connectionString = createConnectionString();
+            Connection connection = DriverManager.getConnection(connectionString);
+            System.out.println("connection class = " + connection.getClass().getName());
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private String createConnectionString() {
+        Properties prop = PropertiesUtil.loadProperty(DB_PROPERTIES);
+        String host = prop.getProperty(DB_HOST);
+        String db = prop.getProperty(DB_NAME);
+        String port = prop.getProperty(DB_PORT);
+        String user = prop.getProperty(DB_USER);
+        String password = prop.getProperty(DB_PASSWORD);
+
+        String connectionString = "jdbc:postgresql://" + host + ":" + port + "/"
+                + db + "?user=" + user + "&password=" + password;
+        System.out.println("connectionString = " + connectionString);
+        return connectionString;
+    }
 }

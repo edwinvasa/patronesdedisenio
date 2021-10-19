4bc08c44b01d595c85c31c098c445ac4896802ec
Edwin Vanegas <edwinvasa@gmail.com>
2021-10-18 21:36:48 -0500

    se crea DBFactory.java utilizada para fabricar los adaptadores de conexión o los subtipos de IDBAdapter como PostgresAdapter y MySQLAdapter. se define los metos getDBAdapter el cual nos permite solicitarle explicitamente que tipo de conexión deseamos por medio del parametro dbType el cual esta definido como un Enum para elegir entre PostgreSQL y MySQL. el metodo getDefaultDBAdapter nos permite obtener una instancia de IDBAdapter previamente configurada, esto, gracias al archivo de configuración DBFactory.properties, una vez cargado el nombre de la clase esta es instanciada y devuelta por el Factory, util cuando podemos preconfigurar que tipo de conexion estaremos utilizando durante la ejecucion.

---
+++ factory/src/DBFactory.java
@@ -0,0 +1,35 @@
+import db.IDBAdapter;
+import impl.MySQLAdapter;
+import impl.PostgresAdapter;
+import util.PropertiesUtil;
+
+import java.util.Properties;
+
+public class DBFactory {
+
+    private static final String DB_FACTORY_URL = "META-INF/DBFactory.properties";
+    private static final String DEFAULT_DB_CLASS = "defaultDBClass";
+
+    public static IDBAdapter getDBAdapter(DBType dbType){
+        switch(dbType) {
+            case MySQL:
+                return new MySQLAdapter();
+            case PostgreSQL:
+                return new PostgresAdapter();
+            default:
+                throw new IllegalArgumentException("No esta soportado");
+        }
+    }
+
+    public static IDBAdapter getDefaultDBAdapter(){
+        try{
+            Properties prop = PropertiesUtil.loadProperty(DB_FACTORY_URL);
+            String defaultDBClass = prop.getProperty(DEFAULT_DB_CLASS);
+            System.out.println("defaultDBClass = " + defaultDBClass);
+            return (IDBAdapter) Class.forName(defaultDBClass).newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
